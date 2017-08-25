c68f624cb26598978c541334d482d27e586d291f
sVujke <stefanvujovic93@gmail.com>
2017-08-25 14:08:57 +0200

    genderise ready

--- genderize_query.py
+++ genderize_query.py
@@ -4,10 +4,17 @@ from genderize import Genderize
 # Genderize client can be downloaded from:
 # https://pypi.python.org/pypi/Genderize
 
-genderize = Genderize(
-    user_agent='GenderizeDocs/0.0',
-    api_key='ebebc52f3b62785a2736f670eba79cad')  # API key
+# TIPS:
+# 1. Remove all names that are just initials
+# 2. Remove first names containing two words like "John Wiliam"
 
+def get_genderize(api_key):
+    """ Returns genderize object, useful when using this script as a module
+    """
+    genderize = Genderize(
+    user_agent='GenderizeDocs/0.0',
+    api_key=api_key)
+    return genderize  
 
 def fetch_gender(name_list, genderize_obj):
     """Return a list of dictionaries, a dictionary for each name in the passed list
@@ -21,31 +28,57 @@ def fetch_gender(name_list, genderize_obj):
     gender_list_flat = [item for sublist in gender_list for item in sublist]
     return gender_list_flat
 
+def male_female(dic,thres):
+    if dic == None:
+        return dic
+    if dic["gender"] == 'male' and dic["probability"] > thres:
+        dic['gender'] = 'male'
+    elif dic["gender"] == 'female' and dic["probability"] > thres:
+        dic['gender'] = 'female'
+    else:
+        dic['gender'] = 'unknown'
+    return dic
+
+def get_dataframe_thres(flat_list,thres):
+    """ Returns pandas dataframe, where every row is a dictionary from the flat list.
+        In this case it takes into account the confidence and also assigns unknown gender
+    """ 
+    lst = []
+    for i in flat_list:
+        new_dic = male_female(i,thres)
+        lst.append(new_dic)
+    df = pd.DataFrame(lst)
+    return df
+
 def get_dataframe(flat_list):
     """ Returns pandas dataframe, where every row is a dictionary from the flat list
     """
     return pd.DataFrame(flat_list)
 
-def get_stats(df,col):
+
+def get_stats(df, col):
     """ Returns a dictionary with number absolute and relative frequency
     """
-    total =len(df)
-    dic =  df[col].value_counts().to_dict()
-    for k,v in dic.items():
-        dic[k] = [v,round(v/total,2)]
+    total = len(df)
+    dic = df[col].value_counts().to_dict()
+    for k, v in dic.items():
+        dic[k] = [v, round(v / total, 2)]
     return dic
 
-def load_file(path,col,typ="json"):
+
+def load_file(path, col, typ="json"):
     """ Returns list of unique names from a specified file, and column
     """
     if typ == "csv":
         df = pd.read_csv(path)
     else:
         df = pd.read_json(path)
+    print("Loading: {}".format(input_path))
     name_list = df[col].unique()
     return name_list
 
-def save_file(df,path,typ="json"):
+
+def save_file(df, path, typ="json"):
     """Saves dataframe as file in specified format:
        JSON or CSV
     """
@@ -53,23 +86,29 @@ def save_file(df,path,typ="json"):
         df.to_csv(path)
     else:
         df.to_json(path)
-    print("File saved at: "+path)
+    print("File saved at: " + path)
 
 if __name__ == "__main__":
 
+    # command line arguments for input and output file paths
+    # input_path "input.csv"
+    # output_path "output.csv"
     input_path = sys.argv[1]
     output_path = sys.argv[2]
 
+    genderize = get_genderize('ebebc52f3b62785a2736f670eba79cad')
+
     # example
 
-    name_list = load_file(input_path,"name")
+    name_list=load_file(input_path, "name", "csv")
 
     print("Fetching gender...")
 
-    flat_list = fetch_gender(name_list,genderize)
+    flat_list=fetch_gender(name_list, genderize)
 
-    df = get_dataframe(flat_list)
+    df=get_dataframe(flat_list)
+    # df=get_dataframe_thres(flat_list,0.8)
 
-    print("Stats: {}".format(get_stats(df,"gender")))
+    print("Stats: {}".format(get_stats(df, "gender")))
 
-    save_file(df,output_path,"csv")
+    save_file(df, output_path, "csv")
